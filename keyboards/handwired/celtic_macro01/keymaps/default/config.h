550016e1cfa7b8d68f70ab7952fb11f24b8d2e29
Jao247 <jao247@live.ie>
2026-04-19 16:39:30 +0100

    Updating celtic_macro01

---
+++ keyboards/handwired/celtic_macro01/keymaps/default/config.h
@@ -0,0 +1,5 @@
+#pragma once
+
+
+#define JOYSTICK_BUTTON_COUNT 0
+#define JOYSTICK_AXIS_COUNT 2
--- keyboards/handwired/celtic_macro01/keymaps/default/keymap.c
+++ keyboards/handwired/celtic_macro01/keymaps/default/keymap.c
@@ -3,22 +3,73 @@
 
 #include QMK_KEYBOARD_H
 
+#define _DEAD 50
+
+#define _DOWN_TRESHOLD (0+_DEAD)
+#define _UP_TRESHOLD (0-_DEAD)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      * ┌────┬────┬────┬────┬────┬────┐
-     * │  7 │ 8  │  9 │  / │    │    │
+     * │  7 │  8 │  9 │  0 │  - │  = │
      * ├────┼────┼────┼────┼────┼────┤
-     * │  4 │ F1 │ F2 │ F3 │ F4 │    │
+     * │ Tab│ F1 │ F2 │ F3 │ F4 │ End│
      * ├────┼────┼────┼────┼────┼────┤
-     * │  1 │ F5 │ F6 │ F7 │ F8 │    │
+     * │  ^ │ F5 │ F6 │ F7 │ F8 │ Del│
      * ├────┼────┼────┼────┼────┼────┤
-     * │  0 │ F9 │ F10│ F11│ F12|    │
+     * │Ctrl│ F9 │ F10│ F11│ F12| Ins│
      * └────┴────┴────┴────┴────┴────┘
      */
     [0] = LAYOUT(
-        KC_P7,   KC_P8,   KC_P9,   KC_PSLS,
-        KC_P4,   KC_P5,   KC_P6,   KC_PAST,
-        KC_P1,   KC_P2,   KC_P3,   KC_PMNS,
-        KC_P0,   KC_PDOT, KC_PENT, KC_PPLS
+        KC_7,      KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,
+        KC_TAB,    KC_F1,   KC_F2,   KC_F3,    KC_F4,    KC_END,
+        KC_LSFT,   KC_F5,   KC_F6,   KC_F7,    KC_F8,    KC_DEL,
+        KC_LCTL,   KC_F9,   KC_F10,  KC_F11,   KC_F12,   KC_INS
     )
 };
+
+
+joystick_config_t joystick_axes[JOYSTICK_AXIS_COUNT] = {
+    JOYSTICK_AXIS_IN(F4, 0, 512, 1023),
+    JOYSTICK_AXIS_IN(F5, 0, 512, 1023)
+};
+
+    int16_t xPos = 0;
+    int16_t yPos = 0;
+
+    bool yDownHeld = false;
+    bool yUpHeld = false;
+    bool xLeftHeld = false;
+    bool xRightHeld = false;
+
+    void matrix_scan_user(void){
+        yPos = joystick_read_axis(1);
+        if (!yDownHeld && yPos >= _DOWN_TRESHOLD) {
+            register_code(KC_UP);
+            yDownHeld = true;
+        } else if (yDownHeld && yPos <= _DOWN_TRESHOLD) {
+            yDownHeld = false;
+            unregister_code(KC_UP);
+        } else if (!yUpHeld && yPos <= _UP_TRESHOLD){
+            yUpHeld = true;
+            register_code(KC_DOWN);
+        }  else if (yUpHeld && yPos > _UP_TRESHOLD){
+            yUpHeld = false;
+            unregister_code(KC_DOWN);
+        }
+
+        xPos = joystick_read_axis(0);
+        if (!xLeftHeld && xPos >= _DOWN_TRESHOLD) {
+            register_code(KC_RGHT);
+            xLeftHeld = true;
+        } else if (xLeftHeld && xPos < _DOWN_TRESHOLD) {
+            xLeftHeld = false;
+            unregister_code(KC_RGHT);
+        } else if (!xRightHeld && xPos <= _UP_TRESHOLD){
+            xRightHeld = true;
+            register_code(KC_LEFT);
+        }  else if (xRightHeld && xPos > _UP_TRESHOLD){
+            xRightHeld = false;
+            unregister_code(KC_LEFT);
+        }
+    }
